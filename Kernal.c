9e2a14d7bf01b9ddefe2658cbfce2c22644ba619
shre-blip <pandeyshreya9967@gmail.com>
2024-09-20 01:57:32 +0530

    Create Kernal.c

---
+++ Kernal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void kernel_main() {
+    printf("Welcome to My UNIX-style OS!\n");
+    // Initialize subsystems like memory, processes, etc.
+}
+
+int main() {
+    kernel_main();
+    return 0;
+}
